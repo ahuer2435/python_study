fbe4a1b191dfb95c541e08a650eb5572a141b8b9
ahuer2435 <18221079843@139.com>
2017-10-11 17:00:48 +0800

    add sort sample.

---
+++ ch05/sort.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+def reversed_cmp(x,y):
+	if x < y:
+		return 1
+	elif x > y:
+		return -1
+	return 0
+
+print sorted([2,4,5,3,2,7,8,12,0],reversed_cmp)
+
+def ignore_case_cmp(x,y):
+	s1 = x.upper()
+	s2 = y.upper()
+	if s1 < s2:
+		return -1
+	elif s1 > s2:
+		return 1
+	return 0
+
+print sorted(['Atut','dtydyt','sgsg','vui','GUGj'],ignore_case_cmp)
+
+# 高阶函数sorted接受其他函数作为参数实例
