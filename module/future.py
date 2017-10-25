d988b51f9da4f2464485ae52092bc9c7b8967585
ahuer2435 <18221079843@139.com>
2017-10-25 10:17:50 +0800

    add futrue test.

---
+++ module/future.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#from __future__ import division
+print '10 / 3 = ', 10 / 3				#python 2.7为3，python3为3.33333
+print '10.0 / 3 = ', 10.0 / 3			#python 2.7和3 以之
+print '10 // 3 =', 10 // 3				#python 2.7和3 以之
+
+#__futrue__关键字用于在低版本引入高版本的特性，似乎没有反向的，似乎对于移植也没有多少好处。
+
